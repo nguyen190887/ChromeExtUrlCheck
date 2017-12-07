1c792243d08f0066d4ecc8da402da701099e56d1
Tri-Nguyen <tri.nguyen@kbb.com>
2017-12-07 14:42:21 +0700

    Open urls when click extension button

--- content.js
+++ content.js
@@ -1,6 +1,19 @@
 var logDomain = '';
-[].forEach.call(document.querySelectorAll('a[href^="http"], a[href^="/"]'), function (item, index){
-    if (!logDomain || item.href.indexOf(logDomain) > -1) {
-        console.log('Url found: ' + item.href);
+
+// listen message
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "clicked_browser_action" ) {
+            var allUrls = 
+                [].map.call(document.querySelectorAll('a[href^="http"], a[href^="/"]'), (item) => {
+                    if (!logDomain || item.href.indexOf(logDomain) > -1) {
+                        return item.href;
+                    }
+                    return '';
+                })
+                .filter(item => item !== '');
+
+            chrome.runtime.sendMessage({"message": "open_new_tab", "urls": allUrls.slice(0, 5)});
+        }
     }
-});
+);
